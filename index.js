baa31ed64d5c0fddc8e333d439f59fbcecc1123e
Boluwatife <boluwatife1904@gmail.com>
2023-07-04 08:17:28 +0100

    reduced entry of numbers to one input field

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* Get the value from the input box */
 let a = document.querySelector("#box-1");
-let b = document.querySelector("#box-2");
-let c = document.querySelector("#box-3");
-let d = document.querySelector("#box-4");
+// let b = document.querySelector("#box-2");
+// let c = document.querySelector("#box-3");
+// let d = document.querySelector("#box-4");
 // let e = document.querySelector("#box-5");
 const guessmsg = document.querySelector('.alert')
 
@@ -26,18 +26,18 @@ function getRandomNumber(a, b, c, d) {
 
 
 function confirmAnswer() {
-let numbers = [Number(a.value), Number(b.value), Number(c.value), Number(d.value)];
+// let numbers = [Number(a.value), Number(b.value), Number(c.value), Number(d.value)];
+let numbers = a.value.split('')
+const newNumbers = numbers.map((curr)=> Number(curr))
 
 let deadArrComp = []
 let injuredArrComp = []
 
 hiddenNumber.forEach((i, idx)=> {
-
-
- let deadArr = numbers.filter((el, index)=>{
+ let deadArr = newNumbers.filter((el, index)=>{
 return i === el && idx === index
 })
- let injuredArr = numbers.filter((el, index)=>{
+ let injuredArr = newNumbers.filter((el, index)=>{
 return i === el && idx !== index
 })
 
@@ -52,13 +52,14 @@ let injuredNum = injuredArrComp.flat().length
 
   console.log(deadNum)
   console.log(injuredNum)
+  console.log(newNumbers);
   console.log(numbers);
 
   guessmsg.classList.add("active")
 
 //  checking if there a number is recurring in the input
-  if(numbers[0] === numbers[1] || numbers[0] === numbers[2] || numbers[0] === numbers [3]
-    || numbers[1] === numbers[2] || numbers[1] === numbers [3] || numbers[2] === numbers [3]
+  if(newNumbers[0] === newNumbers[1] || newNumbers[0] === newNumbers[2] || newNumbers[0] === newNumbers [3]
+    || newNumbers[1] === newNumbers[2] || newNumbers[1] === newNumbers [3] || newNumbers[2] === newNumbers [3]
     ) {
         guessmsg.textContent = `Repeating Numbers detected!`
     } else {
